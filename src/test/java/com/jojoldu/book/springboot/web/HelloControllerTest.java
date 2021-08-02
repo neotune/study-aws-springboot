e0d0e558715674dfa223a3f71976149b12f0acee
neotune <mybluepeter@gmail.com>
2021-08-02 11:02:24 +0900

    gradle7에 맞춰서 변경, junit5에 맞춰서 변경

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,18 +1,30 @@
 package com.jojoldu.book.springboot.web;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
+/*
+    @WebMvcTest는 JPA 설정을 지원하는 기능이 없음
+    Application.java에 @EnableJpaAuditing 기능을 활성화 할 경우 JPA 기능을 지원하지 않는 @WebMvcTest는 아래와 같이 에러를 낸다.
+    Caused by: org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'jpaMappingContext':
+    Invocation of init method failed; nested exception is java.lang.IllegalArgumentException: JPA metamodel must not be empty!
+    해결 할 수 있는 방법은 2가지
+    1. JpaAuditing용 configuration.java 파일 생성 후 @Configuration, @EnableJpaAuditing 어노테이션 추가
+    2. 테스트 파일에 @MockBean 추가
+ */
+@MockBean(JpaMetamodelMappingContext.class)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
