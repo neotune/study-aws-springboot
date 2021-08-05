1b7117300e40c588ddf4563c6965fbb70c12f3d1
neotune <mybluepeter@gmail.com>
2021-08-05 11:14:26 +0900

    [chapter08 EC2 서버에 프로젝트를 배포해 보자] deploy.sh 등록

--- script/deploy.sh
+++ script/deploy.sh
@@ -23,9 +23,7 @@ cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
-
-echo "현재 구동중인 애플리케이션 PID : $CURRENT_PID"
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
